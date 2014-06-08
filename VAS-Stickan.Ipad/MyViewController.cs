ab3bb7ec962ea29cfed6e6d1e48d39612cd22faa
Patrik Lindstrom <patriklindstrom@hotmail.com>
2014-06-08 15:27:33 +0200

    Label slider button works
    All three components work.

--- VAS-Stickan.Ipad/MyViewController.cs
+++ VAS-Stickan.Ipad/MyViewController.cs
@@ -7,6 +7,10 @@ namespace VAS_Stickan.Ipad
     public class MyViewController : UIViewController
     {
         UIButton button;
+         UISlider slider;
+         UILabel label;
+        float sliderWidth = 200;
+        float sliderHeight = 50;
         int numClicks = 0;
         float buttonWidth = 200;
         float buttonHeight = 50;
@@ -22,9 +26,19 @@ namespace VAS_Stickan.Ipad
             View.Frame = UIScreen.MainScreen.Bounds;
             View.BackgroundColor = UIColor.White;
             View.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleHeight;
+            label = new UILabel(new RectangleF(10, 10, 300, 30));
+            label.Text = "Your Pain";
+            View.Add(label);
 
+            slider = new UISlider(new RectangleF(100, 30, 210, 20));
+            View.Add(slider);
+            //    UISlider.Appearance.MinimumTrackTintColor = UIColor.Orange;
+            //UISlider.Appearance.MaximumTrackTintColor = UIColor.Yellow;           
+            slider.MinValue = 0f;
+            slider.MaxValue = 10f;
+            slider.Value = 2.5f; // initial value
             button = UIButton.FromType(UIButtonType.RoundedRect);
-
+            slider.ValueChanged += HandleValueChanged;
             button.Frame = new RectangleF(
                 View.Frame.Width / 2 - buttonWidth / 2,
                 View.Frame.Height / 2 - buttonHeight / 2,
@@ -44,6 +58,13 @@ namespace VAS_Stickan.Ipad
             View.AddSubview(button);
         }
 
+        private void HandleValueChanged(object sender, EventArgs e)
+        {
+            // display the value in a label
+            label.Text = slider.Value.ToString();
+        }
+
+       
     }
 }
 
